4949f54a39f231c3c49928c4b5d8f286ab24a60b
Nonexdog <63328613+Nonexdog@users.noreply.github.com>
2024-02-04 14:57:49 -0600

    Create mark factory to assign values to grid cells
    We do not want to accidentally set a value that is not 0, X, or O, so creating this function allows for more restricted control of the cell values.

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const gameBoard = (function() {
-  const boardMatrix = [
-    [ [], [], [] ],
-    [ [], [], [] ],
-    [ [], [], [] ]
-  ];
+  const boardMatrix = [];
+
+  for (let row = 0; row < 3; row++) {
+    boardMatrix[row] = [];
+    for (let col = 0; col < 3; col++) {
+      boardMatrix[row][col] = mark();
+    }
+  }
+
+  function mark() {
+    let value = 0;
+
+    const getValue = () => value;
+    const assignValue = (newValue) => {
+      if (newValue === 0 || newValue === 'X' || newValue === 'O') {
+        value = newValue;
+      }
+    }
+
+    return { getValue, assignValue }
+
+  }
 
   const getBoard = () => boardMatrix;
 
